8e8f52acbe71ab65e7799c9942b2c97fa854b6ca
test3620 <kpoutou700@gmail.com>
2024-03-22 19:28:04 +0000

    La mise en ligne de la version 3.1

--- AJRDL/urls.py
+++ AJRDL/urls.py
@@ -1,7 +1,7 @@
 """ IMPORTATION DES MODULES """
 from django.contrib import admin
 from django.urls import path, include 
-from .views import accueil_view, a_propos_view # PAGES : ACCUEIL, A PROPOS 
+from .views import accueil_view, a_propos_view, bureau_view, cotisation_view, actualite_view, inscription_view, contact_view # PAGES : ACCUEIL, A PROPOS, BUREAU, COTISATION, ACTUALITE, INSCRIPTION, CONTACT
 
 
 from django.conf import settings
@@ -12,6 +12,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', accueil_view, name='accueil'), # Le chemin vers la page d'accueil
     path('a propos/', a_propos_view, name='a_propos'), # Le chemin vers la page de à propos
+    path('bureau/', bureau_view, name='bureau'), # Le chemin vers la page du bureau
+    path('cotisation/', cotisation_view, name='cotisation'), # Le chemin vers la page de cotisqtion
+    path('actualite/', actualite_view, name='actualite'), # Le chemin vers la page d'actualité
+    path('inscription/', inscription_view, name='inscription'), # Le chemin vers la page d'inscription
+    path('contact/', contact_view, name='contact'), # Le chemin vers la page de contact
     path('membre/', include('membre.urls')), # Le chemin vers la page des membres
 ]
 
--- AJRDL/views.py
+++ AJRDL/views.py
@@ -7,3 +7,23 @@ def accueil_view(request):
 """ PAGE DE A PROPOS """
 def a_propos_view(request):
     return render(request, 'a_propos.html')
+
+""" PAGE DU BUREAU """
+def bureau_view(request):
+    return render(request, 'bureau.html')
+
+""" PAGE DE COTISATION """
+def cotisation_view(request):
+    return render(request, 'cotisation.html')
+
+""" PAGE D'ACTUALITÉ """
+def actualite_view(request):
+    return render(request, 'actualite.html')
+
+""" PAGE D'INSCRIPTION' """
+def inscription_view(request):
+    return render(request, 'inscription.html')
+
+""" PAGE DE CONTACT """
+def contact_view(request):
+    return render(request, 'contact.html')
